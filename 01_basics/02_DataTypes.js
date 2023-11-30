cc7313ac5df85c1c5d2ce5ae4873339e434db687
PACHITRA <95424607+PACHITRA@users.noreply.github.com>
2023-11-30 13:08:41 +0000

    nest

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -1,1 +1,24 @@
-"use strict";// treat
+"use strict";// treat all the JScode as newer version
+// alert(3+3) // we are using noodejs noot browser
+console.log(3+3)   //code readability is high
+console.log("Pavan")
+
+let name="pavan"
+let age=19
+let isLoggedIn=false
+let state;
+//number =>2^53
+//bigint
+//string =>""
+//boolean =>true/false
+// null => standalone value //it is an object
+// undefined => //undefined is a type not an object
+// symbol=>Uniqueness
+
+
+
+
+
+//object
+
+console.log(typeof null);
