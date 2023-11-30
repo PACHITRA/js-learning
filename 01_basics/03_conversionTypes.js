2ee0577790dc5e5d6d7efeff947950d234e0e552
PACHITRA <95424607+PACHITRA@users.noreply.github.com>
2023-11-30 14:58:52 +0000

    gg

---
+++ 01_basics/03_conversionTypes.js
@@ -0,0 +1,25 @@
+//let score="33"=>33
+//let score="33abc"=>nan
+//let score=null=>0
+//let score=undefined=>nan
+let score=true//=>1
+
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+let isLoggedIn = "pavan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber=33
+let stringnumber=String(someNumber)
